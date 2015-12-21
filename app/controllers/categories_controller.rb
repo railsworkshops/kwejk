d785aae11beffc8f953c610c1e8477b3dd5e8cec
lemon <l3moniak2@gmail.com>
2015-12-21 13:44:47 +0100

    Categories controller clean up

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,79 +1,40 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
-  before_action :get_images, only: :show
+  expose(:category) { set_category }
+  expose(:categories)
+  expose(:images) { category.images }
 
-  # GET /categories
-  # GET /categories.json
-  def index
-    @categories = Category.all
-  end
-
-  # GET /categories/1
-  # GET /categories/1.json
-  def show
-  end
-
-  # GET /categories/new
-  def new
-    @category = Category.new
-  end
-
-  # GET /categories/1/edit
-  def edit
-  end
-
-  # POST /categories
-  # POST /categories.json
   def create
-    @category = Category.new(category_params)
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
   def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if category.update_attributes(category_params)
+      redirect_to categories_path, notice: 'Category was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
   def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    category.destroy
+    redirect_to categories_path, notice: 'Category was successfully destroyed.'
   end
 
   private
-    def get_images
-      @category.images
-    end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find_by_slug(params[:id])
+  def set_category
+    if params[:id].present?
+      Category.find_by(slug: params[:id])
+    else
+      Category.new(category_params)
     end
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def category_params
-      params.require(:category).permit(:name, :slug, :default)
-    end
+  def category_params
+    params.require(:category).permit(:name, :slug, :default) if params[:category]
+  end
 end
